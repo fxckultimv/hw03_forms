c0700d205d71ccf05409a41b6774e6764032fa90
Mikhail Komissarov <datradexon@gmail.com>
2023-03-29 00:49:52 +0300

    PEP8 fixes

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,14 @@
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-
 from .forms import CommentForm, PostForm
 from .models import Follow, Group, Post, User
+from constants import PAGE_LIMIT
 
 
 def paginator(request, post_list):
     """Паджинатор."""
-    paginator = Paginator(post_list, settings.POSTS_PER_PAGE)
+    paginator = Paginator(post_list, PAGE_LIMIT)
     page_number = request.GET.get('page')
     return paginator.get_page(page_number)
 
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -105,8 +105,6 @@ LOGIN_REDIRECT_URL = 'posts:index'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
-POSTS_PER_PAGE = 10
-
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 
 MEDIA_URL = '/media/'
