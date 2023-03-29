fb0bc05b50f8cf6ec38e906ce669d8e6f9d4fff6
Mikhail Komissarov <datradexon@gmail.com>
2023-03-29 11:32:54 +0300

    Minor changes

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -113,7 +113,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache', }}
-
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
